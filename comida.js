57525be4bbf0525dc24f9cad8991e623ad8ed1b7
Yuri Castilho <99267254+YuriCast@users.noreply.github.com>
2022-03-08 12:50:21 -0300

    Add files via upload

---
+++ comida.js
@@ -0,0 +1,19 @@
+x = 1
+nome = "pizza"
+console.log(nome+x)
+
+x = 2
+nome = "peixe"
+console.log(nome+x)
+
+x = 3
+nome = "frango"
+console.log(nome+x)
+
+x = 4
+nome = "carne"
+console.log(nome+x)
+
+x = 5
+nome = "arroz"
+console.log(nome+x)
