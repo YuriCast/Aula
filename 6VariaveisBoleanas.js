81e1c1b000c901518c90713c85c1da70d55a3a79
Yuri Castilho <99267254+YuriCast@users.noreply.github.com>
2022-04-23 16:38:15 -0300

    Create 6VariaveisBoleanas.js

---
+++ 6VariaveisBoleanas.js
@@ -0,0 +1,29 @@
+var isCantando = true;
+if (isCantando) {
+  console.log("estou cantando");
+}
+
+var isPulando = false;
+if (isPulando) {
+  console.log("estou pulando");
+}
+
+var isChorando = true;
+if (isChorando) {
+  console.log("estou chorando");
+}
+
+var isComendo = false;
+if (isComendo) {
+  console.log("estou comendo");
+}
+
+var isLendo = true;
+if (isLendo) {
+  console.log("estou lendo");
+}
+
+var isRindo = false;
+if (isRindo) {
+  console.log("estou rindo");
+}
