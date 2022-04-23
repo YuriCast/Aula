3f538981b7888b6e8fb21440c705b960009facde
Yuri Castilho <99267254+YuriCast@users.noreply.github.com>
2022-04-23 16:33:17 -0300

    Update clientes.js

--- clientes.js
+++ clientes.js
@@ -1,11 +1,11 @@
-x = 1
-nome = "joao"
-console.log(nome+x)
+var x = 1;
+var nome = "Maria";
+console.log(nome + x);
 
-x = 3
-nome = "matheus"
-console.log(nome+x)
+x = 2;
+nome = "Ana";
+console.log(nome + x);
 
-x = 2
-nome = "lucas"
-console.log(nome+x)
+x = 3;
+nome = "José";
+console.log(nome + x);
